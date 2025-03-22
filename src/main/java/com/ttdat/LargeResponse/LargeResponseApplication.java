3b4a5850e34e530bd69bf071ce53648975a726e8
ttdat.99 <55250111+ttdat-thecodeguy@users.noreply.github.com>
2025-03-22 22:45:10 +0700

    first-example

---
+++ src/main/java/com/ttdat/LargeResponse/LargeResponseApplication.java
@@ -0,0 +1,13 @@
+package com.ttdat.LargeResponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LargeResponseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttdat/LargeResponse/LargeResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttdat.LargeResponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeResponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
